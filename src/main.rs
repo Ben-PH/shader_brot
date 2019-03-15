b7527894671f443eb7f2ec52d1a3e6fcc76c3a65
Ben-PHb <benphawke@gmail.com>
2019-03-15 16:11:57 +1100

    Clippy and `unwrap()` cleanup.

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@
 extern crate gfx;
 extern crate cgmath;
 
-use ggez::graphics::{self, DrawMode, Drawable, DrawParam, Canvas};
+use ggez::graphics::{self, DrawParam, Canvas};
 use ggez::Context;
 use ggez::event::{ KeyCode, KeyMods };
 use ggez::GameResult;
@@ -50,9 +50,8 @@ struct MainState {
 }
 
 impl MainState {
-    fn new(ctx: &mut Context) -> Self {
+    fn new(ctx: &mut Context) -> GameResult<Self> {
 
-        let canvas = Canvas::with_window_size(ctx).unwrap();
         let mand = Mandel{
             position: [0.0, 0.0],
             center: [0.0, 0.0],
@@ -62,20 +61,23 @@ impl MainState {
         };
         let thing1 = include_bytes!("../resources/basic_330.glslv");
         let thing2 = include_bytes!("../resources/fractal.glslf");
-        Self {
-            canvas: Canvas::with_window_size(ctx).unwrap(),
+        let canvas = Canvas::with_window_size(ctx)?;
+        let shad = graphics::Shader::from_u8(
+            ctx,
+            thing1,
+            thing2,
+            mand,
+            "Mandel",
+            None
+        )?;
+
+        Ok(Self {
+            canvas,
             center: [0.0, -0.5].into(),
             zoom: 1.0,
             mand,
-            shad: graphics::Shader::from_u8(
-                ctx,
-                thing1,
-                thing2,
-                mand,
-                "Mandel",
-                None
-            ).expect("fail"),
-        }
+            shad,
+        })
     }
 }
 
@@ -109,7 +111,6 @@ impl ggez::event::EventHandler for MainState {
                 None => {},
         }
 
-        const ZOOM_SCALE: f32 = 1.0;
         self.mand.center[0] -= zoom_coeficient.0 / self.mand.zoom;
         self.mand.center[1] -= zoom_coeficient.1 / self.mand.zoom;
         // self.mand.center[0] *= ZOOM_SCALE;
@@ -117,7 +118,7 @@ impl ggez::event::EventHandler for MainState {
 
         match keycode {
             KeyCode::Period => self.mand.zoom *= 1.0 + 1.0/10.0,
-            KeyCode::E => self.mand.zoom *= 1.0 - 1.0/10.00000001,
+            KeyCode::E => self.mand.zoom *= 1.0 - 1.0/10.000_01,
             KeyCode::Q => {
                 println!("MainState\n=========\n {:#?}", &self);
                 ggez::quit(ctx);
@@ -129,17 +130,17 @@ impl ggez::event::EventHandler for MainState {
         graphics::clear(ctx, [0.1, 0.1, 0.3, 1.0].into());
         {
             let _lock = graphics::use_shader(ctx, &self.shad);
-            self.shad.send(ctx, self.mand).unwrap();
+            self.shad.send(ctx, self.mand)?;
             // let foreground = Canvas::with_window_size(ctx)?;
             graphics::draw(ctx, &self.canvas, DrawParam::default())?;
-            graphics::present(ctx).unwrap();
+            graphics::present(ctx)?;
         }
         Ok(())
     }
 
 }
 
-fn main() {
+fn main() -> GameResult {
     let resource_dir = if let Ok(manifest_dir) = env::var("CARGO_MANIFEST_DIR") {
         let mut path = path::PathBuf::from(manifest_dir);
         path.push("resources");
@@ -149,10 +150,10 @@ fn main() {
     };
 
     let cb = ggez::ContextBuilder::new("shader", "moi").add_resource_path(resource_dir);
-    let (ctx, event_loop) = &mut cb.build().unwrap();
+    let (ctx, event_loop) = &mut cb.build()?;
 
-    let mut ms = MainState::new(ctx);
+    let mut ms = MainState::new(ctx)?;
 
-    ggez::event::run(ctx, event_loop, &mut ms);
+    ggez::event::run(ctx, event_loop, &mut ms)
 
 }
