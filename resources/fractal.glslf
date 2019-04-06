1b42e33ed578679054d45dc9b164626135a17d80
Ben-PHb <benphawke@gmail.com>
2019-04-06 16:08:39 +1100

    Clean up to a presentable state.

--- resources/fractal.glslf
+++ resources/fractal.glslf
@@ -4,105 +4,114 @@
 #version 420 core
 
 #define ESCAPE_BOUNDARY 18
-#define FREQUENCY 0.1
+#define FREQUENCY 1.0
 
 in highp vec4 gl_FragCoord;
 out vec4 Target0;
 
 
 dvec2 vec2_to_dvec2(vec2 floatvec2);          // Naive approach to guaranteeing double precision
-dvec2 viewport_coord_to_complex(dvec2 coord); // moves and scales gl_FragCoord to desired complex number
+dvec2 viewport_coord_to_complex(dvec2 coord);
 
 dvec2 mouse_pos_to_complex();
 dvec2 fragment_coord_to_complex();
 
 vec3 hsv2rgb( vec3 c);
 
+// Values defined by the front-end
 layout (std140) uniform MandelShaderUniforms {
-  uniform dvec2 u_Center;     // where on the complex plain the user wants the screen-center
-  uniform dvec2 u_Dimension;  // the width and height user wants the view onto the complex plane
-  uniform dvec2 u_Resolution; // user interogates the pixel coverage of the view, and provides it here
 
-  uniform vec2 u_MousePos;    // Used for julia set, assumes gl_FragCoord format
-  uniform float u_Time;       // Time since start of program in seconds
-  uniform int u_MaxIteration; // User defined limit of iteration count
-  uniform int u_IsMandel;     // (A bit hacky, TODO) select between MandelBrot and Julia
+    // View-port values
+    uniform dvec2 u_Center;     // Center-pixel location
+    uniform dvec2 u_Dimension;  // width-height
+    uniform dvec2 u_Resolution; // Pixel-resolution of viewport
+
+  
+    // exploration controlls
+    uniform vec2 u_MousePos;    // Used for julia-set, and hybrids
+    uniform float u_Time;       // Time elapsed in seconds
+    uniform int u_MaxIteration;
+    uniform int u_IsMandel;     // (A bit hacky, TODO) MandelBrot / Julia swap
 };
 
 
 
 void main() {
 
-  // load up our iteration values
-  dvec2 complex_mouse = mouse_pos_to_complex();
-  dvec2 complex_fragment = fragment_coord_to_complex();
-  dvec2 z, c;
-  if (u_IsMandel == 1) {
-    z = dvec2(0.0, 0.0);
-    c = complex_fragment;
-  } else {
-    z = complex_fragment;
-    c = complex_mouse;
-  }
+    // load up our iteration values
+    dvec2 complex_mouse = mouse_pos_to_complex();
+    dvec2 complex_fragment = fragment_coord_to_complex();
+    dvec2 z, c;
 
-
-  int step_count;
-  dvec2 tmp;
-  for (step_count = 0; step_count < u_MaxIteration; step_count++) {
-
-    // Vast majority of performance gets absorbed here
-
-    // TODO micro-optimise
-    // `z = z * z + c`, with z and c being complex numbers
-
-    tmp.x = z.x*z.x - z.y*z.y;
-    tmp.y = 2.0 * z.x*z.y;
-    z.x = tmp.x;
-    z.y = tmp.y;
-    z.x += c.x;
-    z.y += c.y;
-
-    if((z.x*z.x + z.y*z.y) > ESCAPE_BOUNDARY) {
-      break;
+    // We are looking at mandelbrot set
+    if (u_IsMandel == 1) {
+        z = dvec2(0.0, 0.0);
+        c = complex_fragment;
+    } else {
+        // we are looking at julia set
+        z = complex_fragment;
+        c = complex_mouse;
     }
-  }
 
 
-  if (step_count == u_MaxIteration) {
-    Target0 =  vec4(1.0, 1.0, 1.0, 1.0);
-  } else {
-    float dist = float(length(z));
 
-    float two = float(2.0);
-    dist  = log(log(dist)) / log(two);
 
-    float val = float(step_count) - dist;
-    float hue = val/10.0 - u_Time * FREQUENCY;
+    int step_count;
+    dvec2 tmp;
+    for (step_count = 0; step_count < u_MaxIteration; step_count++) {
 
-    Target0 =  vec4(hsv2rgb(vec3((hue), 1.0, 1.0 )), 1.0);
-  }
+        // Vast majority of performance gets absorbed here
+        // TODO micro-optimise 2019-03-30
+        // `z = z * z + c`, with z and c being complex numbers
+
+        tmp.x = z.x*z.x - z.y*z.y;
+        tmp.y = 2.0 * z.x*z.y;
+        z.x = tmp.x;
+        z.y = tmp.y;
+        z.x += c.x;
+        z.y += c.y;
+
+        if((z.x*z.x + z.y*z.y) > ESCAPE_BOUNDARY) {
+            break;
+        }
+    }
+
+
+    if (step_count == u_MaxIteration) {
+        Target0 =  vec4(1.0, 1.0, 1.0, 1.0);
+    } else {
+        float dist = float(length(z));
+
+        float two = float(2.0);
+        dist  = log(log(dist)) / log(two);
+
+        float val = float(step_count) - dist;
+        float hue = val/10.0 - u_Time * FREQUENCY;
+
+        Target0 =  vec4(hsv2rgb(vec3((hue), 1.0, 1.0 )), 1.0);
+    }
 }
+
 // enhance...
 dvec2 vec2_to_dvec2(vec2 floatvec2) {
-  return dvec2(floatvec2);
+    return dvec2(floatvec2);
 }
 
 // center yourself, THEN grow your mind, then you will be moved on the plane of imagination
 dvec2 viewport_coord_to_complex(dvec2 coord) {
 
-  // set the origin to the center of the screen
-  dvec2 result =  dvec2(coord) - u_Resolution/2.0;
+    // set the origin to the center of the screen
+    dvec2 result =  dvec2(coord) - u_Resolution/2.0;
 
+    // Scale down to a 1.0 by 1.0 view of the complex plane
+    result /= u_Resolution;
 
-  // Scale down to a 1.0 by 1.0 view of the complex plane
-  result /= u_Resolution;
+    // match the real and im dimensions to user-input
+    result *= u_Dimension;
 
-  // match the real and im dimensions to user-input
-  result *= u_Dimension;
-
-  // shift the center reference to where the user asked it to be
-  result += u_Center;
-  return result;
+    // shift the center reference to where the user asked it to be
+    result += u_Center;
+    return result;
 }
 
 
@@ -119,7 +128,7 @@ dvec2 mouse_pos_to_complex() {
 
 // hue, saturation, value to reg, green, blue
 vec3 hsv2rgb(vec3 c) {
-  vec4 K =  vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
-  vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www );
-  return c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);
+    vec4 K =  vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
+    vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www );
+    return c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);
 }
--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ use ggez::Context;
 use ggez::event::{ KeyCode, KeyMods };
 use ggez::GameResult;
 
-use std::env;
+
 use std::path;
 
 mod direction;
@@ -23,7 +23,7 @@ gfx_defines! {
 
     // DONE set it up so that you can idiomatically select for mandel or julia
     // TODO refactor so more idiomatic in the code structure
-    constant Mandel {
+    constant MandelShaderUniforms {
         center: [f64; 2] = "u_Center",
         dimension: [f64; 2] = "u_Dimension",
         resolution: [f64; 2] = "u_Resolution",
@@ -34,10 +34,10 @@ gfx_defines! {
     }
 }
 
-impl Mandel {
+impl MandelShaderUniforms {
     fn new(ctx: &Context) -> Self {
         Self {
-            // TODO varify that these uniforms are set in cpu only
+            // DONE varify that these uniforms are set in cpu only
             // TODO delegate operations on these uniforms
             position: [0.0, 0.0],
             center: [-0.5, -0.0],
@@ -55,11 +55,12 @@ impl Mandel {
 #[derive (Debug)]
 struct MainState {
     // TODO collect these items in a refactor
+    // TODO decouple variable type and name
     canvas_render_target: Canvas,
     zoom: f64,
 
-    mandel_uniforms: Mandel,
-    shader: graphics::Shader<Mandel>,
+    uniforms_for_shader: MandelShaderUniforms,
+    shader: graphics::Shader<MandelShaderUniforms>,
 }
 
 
@@ -70,45 +71,40 @@ impl MainState {
 
         let canvas_render_target = Canvas::with_window_size(ctx)?;
 
-        let mandel_uniforms = Mandel::new(ctx);
+        // We need to hava a data struct to send to the shader.
+        let uniforms_for_shader = MandelShaderUniforms::new(ctx);
 
         let shader = graphics::Shader::from_u8(
             ctx,
-            // vertex source-code
+            // Import vertex and fragment shader source-code at compile time
             include_bytes!("../resources/basic_330.glslv"),
-            // fragment source-code
             include_bytes!("../resources/fractal.glslf"),
-            mandel_uniforms,
-            "Mandel",
+            uniforms_for_shader,
+            "MandelShaderUniforms",
             None
         )?;
 
-
-        // bring it all together
+        // Bring together the target, uniforms, and shader into the MainState
         Ok(Self {
             canvas_render_target,
             zoom: 1.0,
-            mandel_uniforms,
+            uniforms_for_shader,
             shader,
         })
-    }
-
-    fn set_origin(&mut self, center: [f64; 2]) {
-        self.mandel_uniforms.center = center;
     }
 
     // TODO command depends on this. change so this depends on command
     // by doing += argument
     fn incriment_max_iter(&mut self) {
-        self.mandel_uniforms.max_iter += ITER_STEP;
+        self.uniforms_for_shader.max_iter += ITER_STEP;
     }
 
     // TODO command depends on this. change so this depends on command
     // by doing += argument
     fn decriment_max_iter(&mut self) {
-        let it = self.mandel_uniforms.max_iter;
+        let it = self.uniforms_for_shader.max_iter;
         let it = std::cmp::max(2, it - ITER_STEP);
-        self.mandel_uniforms.max_iter = it;
+        self.uniforms_for_shader.max_iter = it;
     }
 }
 
@@ -118,7 +114,7 @@ impl ggez::event::EventHandler for MainState {
         // time since last frame to provide to the shader
         let shader_time = ggez::timer::time_since_start(ctx);
         let shader_time = ggez::timer::duration_to_f64(shader_time) * SPEED_SCALE;
-        self.mandel_uniforms.time = shader_time as f32;
+        self.uniforms_for_shader.time = shader_time as f32;
 
         Ok(())
     }
@@ -128,7 +124,7 @@ impl ggez::event::EventHandler for MainState {
         // We want everything to scale according to a screen-pixel. With a new
         // window size, the shader needs new information on pixel resolution
         let os_scale = graphics::os_hidpi_factor(ctx);
-        self.mandel_uniforms.resolution = [(width*os_scale) as f64, (height*os_scale) as f64];
+        self.uniforms_for_shader.resolution = [(width*os_scale) as f64, (height*os_scale) as f64];
     }
 
     fn draw(&mut self, ctx: &mut Context) -> GameResult {
@@ -139,7 +135,7 @@ impl ggez::event::EventHandler for MainState {
         // have the gpu paint to a clean canvas without interuption, then unveil it.
         {
             let _lock = graphics::use_shader(ctx, &self.shader);
-            self.shader.send(ctx, self.mandel_uniforms)?;
+            self.shader.send(ctx, self.uniforms_for_shader)?;
             graphics::draw(ctx, &self.canvas_render_target, DrawParam::default())?;
             graphics::present(ctx)?;
         }
@@ -160,8 +156,8 @@ impl ggez::event::EventHandler for MainState {
         let scale = graphics::os_hidpi_factor(ctx);
 
         let y = graphics::size(ctx).1 as f32  - y;
-        self.mandel_uniforms.position[0] = x * scale;
-        self.mandel_uniforms.position[1] = y * scale;
+        self.uniforms_for_shader.position[0] = x * scale;
+        self.uniforms_for_shader.position[1] = y * scale;
     }
 
     fn key_down_event(
@@ -172,41 +168,36 @@ impl ggez::event::EventHandler for MainState {
         _repeat: bool,
     ) {
 
-        // TODO work out how to decouble responsibility
-        let mut zoom_coeficient = (0.0, 0.0);
         // TODO move this into a function call
         match Direction::from_keycode(keycode) {
-            Some(Direction::Up)    => self.mandel_uniforms.center[1] += self.mandel_uniforms.dimension[1] * 0.2,
-            Some(Direction::Down)  => self.mandel_uniforms.center[1] -= self.mandel_uniforms.dimension[1] * 0.2,
-            Some(Direction::Left)  => self.mandel_uniforms.center[0] -= self.mandel_uniforms.dimension[0] * 0.2,
-            Some(Direction::Right) => self.mandel_uniforms.center[0] += self.mandel_uniforms.dimension[0] * 0.2,
+            Some(Direction::Up)    => self.uniforms_for_shader.center[1] += self.uniforms_for_shader.dimension[1] * 0.2,
+            Some(Direction::Down)  => self.uniforms_for_shader.center[1] -= self.uniforms_for_shader.dimension[1] * 0.2,
+            Some(Direction::Left)  => self.uniforms_for_shader.center[0] -= self.uniforms_for_shader.dimension[0] * 0.2,
+            Some(Direction::Right) => self.uniforms_for_shader.center[0] += self.uniforms_for_shader.dimension[0] * 0.2,
             None => {},
         }
 
-        // TODO make this redundant then delete
-        self.mandel_uniforms.center[0] += (zoom_coeficient.0 / self.zoom) as f64;
-        self.mandel_uniforms.center[1] += (zoom_coeficient.1 / self.zoom) as f64;
 
         // TODO add comments to template high-level representation of planned commands
         match keycode {
             KeyCode::E => {
-                self.mandel_uniforms.dimension[0] *= 0.9;
-                self.mandel_uniforms.dimension[1] *= 0.9;
+                self.uniforms_for_shader.dimension[0] *= 0.9;
+                self.uniforms_for_shader.dimension[1] *= 0.9;
             }
             KeyCode::W => self.incriment_max_iter(),
             KeyCode::S => self.decriment_max_iter(),
             KeyCode::D => {
-                self.mandel_uniforms.dimension[0] *= 1.1;
-                self.mandel_uniforms.dimension[1] *= 1.1;
+                self.uniforms_for_shader.dimension[0] *= 1.1;
+                self.uniforms_for_shader.dimension[1] *= 1.1;
             },
             KeyCode::Q => {
                 println!("MainState\n=========\n {:#?}", &self);
                 ggez::quit(ctx);
             },
             KeyCode::Tab => {
-                match self.mandel_uniforms.is_mandel {
-                    0 => self.mandel_uniforms.is_mandel = 1,
-                    _ => self.mandel_uniforms.is_mandel = 0,
+                match self.uniforms_for_shader.is_mandel {
+                    0 => self.uniforms_for_shader.is_mandel = 1,
+                    _ => self.uniforms_for_shader.is_mandel = 0,
                 }
             },
             _ => {}
@@ -218,6 +209,7 @@ fn main() -> GameResult {
     let resource_dir = path::PathBuf::from("./resources");
 
     let cb = ggez::ContextBuilder::new("shader-driven julia/mandelbrot", "BenPH").add_resource_path(resource_dir).with_conf_file(true);
+
     let (ctx, event_loop) = &mut cb.build()?;
     ctx.conf.window_mode = ggez::conf::WindowMode::resizable(ctx.conf.window_mode, true);
 
